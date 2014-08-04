66240d52d82f96da6c18fb0285d166bd39c0aad2
PGMacDesign <pgmacdesign@gmail.com>
2014-08-04 02:34:12 -0700

    Stuff

--- src/com/pgmacdesign/rsrtoolbox/ETFCalculator.java
+++ src/com/pgmacdesign/rsrtoolbox/ETFCalculator.java
@@ -1,5 +1,21 @@
 package com.pgmacdesign.rsrtoolbox;
+/*
+        Copyright (C) <2014>  <Patrick Gray MacDowell>
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+ 
 //This class calculates the early termination fee of a customer's line 
 public class ETFCalculator {
 
